49e124472f431f8f74e1fe8688b0e0b8d37d6034
Phil Hart <phil@Phils-iMac.local>
2018-11-16 09:08:31 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const port = 8000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(port, () => console.log(`Server listening on ${port}`));
